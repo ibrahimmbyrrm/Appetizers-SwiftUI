42d1959aabd2dfab540dd460aec9aa12e743b851
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-09-13 18:13:58 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by İbrahim Bayram on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
